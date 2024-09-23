e4ae12ebf931dcbbef7373c726f364ac9b1d4bb7
otinu <fitshter@gmail.com>
2024-09-23 11:08:40 +0900

    Lesson2

--- Swift-Beginners/Swift-Beginners/ContentView.swift
+++ Swift-Beginners/Swift-Beginners/ContentView.swift
@@ -1,21 +1,23 @@
-//
-//  ContentView.swift
-//  Swift-Beginners
-//
-//  Created by Apple on 2024/09/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var outputText = "Hello World"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(outputText)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(Color.orange)
+            Button("切り替えボタン") {
+                outputText = "Nice👌"
+            }
+            // padding()が上にあるか下にあるかで、レイアウトに違いが出てくる
+            // →レイアウトにつまづいたら、装飾メソッド(?)の呼び出し順も注意
+            .padding(.all)
+            .background(.purple)
+            .foregroundColor(.white)
         }
-        .padding()
     }
 }
 
